344ea49b0683499ae77d5485190242edba70620b
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2020-10-16 15:24:28 +0200

    Bit of enhancements

--- setup.sh
+++ setup.sh
@@ -3,18 +3,19 @@
 
 echo "Install pyenv"
 curl https://pyenv.run | bash
-
+which pyenv
 pyenv update
 
 echo "Install lazydocker"
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+which lazydocker
 
-
-COMPOSE_VERSION = "1.27.4"
+COMPOSE_VERSION="1.27.4"
 echo "Installing docker-compose $COMPOSE_VERSION"
 
 ## Install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+which docker-composet
 
 echo "Installed"
