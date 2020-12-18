bdbcd99a5e1b3354b17457b7c5c9c387aaaaf3aa
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2020-12-18 19:13:36 +0100

    moved git to separate setup
    Add cleanup-remote alias

---
+++ setup-git.sh
@@ -0,0 +1,9 @@
+echo "Configure git"
+echo "Set git attributes"
+# Based on https://tekin.co.uk/2020/10/better-git-diff-output-for-ruby-python-elixir-and-more
+# Existing diffs https://github.com/git/git/blob/master/userdiff.c
+cp .gitattributes ~/.gitattributes
+git config --global core.attributesfile ~/.gitattributes
+git config --global alias.cleanup "!git branch --merged | grep  -v '\\*\\|master\\|dev' | xargs -n 1 git branch -d"
+git config --global alias.cleanup-remote "!git fetch origin -p && git branch -r --list 'origin/*' --merged origin/master | egrep -v '(^\*|master|dev)' | sed 's/origin\///' | xargs -n 1 git push origin --delete"
+echo "Git configuration completed"
--- setup.sh
+++ setup.sh
@@ -22,11 +22,4 @@ which docker-composet
 echo "Installed"
 
 
-echo "Configure git"
-echo "Set git attributes"
-# Based on https://tekin.co.uk/2020/10/better-git-diff-output-for-ruby-python-elixir-and-more
-# Existing diffs https://github.com/git/git/blob/master/userdiff.c
-cp .gitattributes ~/.gitattributes
-git config --global core.attributesfile ~/.gitattributes
-git config --global alias.cleanup "!git branch --merged | grep  -v '\\*\\|master\\|dev' | xargs -n 1 git branch -d"
-echo "Git configuration completed"
+./setup-git.sh
