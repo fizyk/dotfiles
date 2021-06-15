64a13baf32f5f511b7b1c64a4d5bd8134c157460
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2021-06-15 16:14:34 +0200

    Install pyenv inside zshrc

--- setup.sh
+++ setup.sh
@@ -8,16 +8,27 @@ fi
 
 ./setup-zsh.sh
 
-echo "Install pyenv"
-curl https://pyenv.run | bash
+if [ -d "$HOME/.pyenv/" ]; then
+    pyenv update
+    echo "Oh-My-Zsh installed, run omz update in terminal"
+else
+    echo "Install pyenv"
+    curl https://pyenv.run | bash
+fi
+
+if ! grep -Fxq 'eval "$(pyenv init -)"' "$HOME/.zshrc"
+then
+    echo "Configuring pyenv for zsh"
+    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+fi
 which pyenv
-pyenv update
 
 echo "Install lazydocker"
 # https://github.com/jesseduffield/lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 which lazydocker
 
+# TODO: install golang if not exists
 echo "Install golangci-lint"
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.39.0
 which golangci-lint
