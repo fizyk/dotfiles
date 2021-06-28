fc2ab78b066523770fd8e9ce6e3a8be179b54bd3
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2021-06-28 15:01:45 +0200

    Bit comments on what to do as well

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -12,10 +12,15 @@ else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     echo "Install powerlevel10k"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    # Set ZSH_THEME="powerlevel10k/powerlevel10k"
     echo "Install zsh-syntax-highlighting"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    # https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md#oh-my-zsh
     echo "Install zsh-autosuggestions"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    # https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh
     echo "Go to https://github.com/ryanoasis/nerd-fonts/releases and select proper fonts, install it in the system and configure your terminal to use it."
+    # https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip
 fi
 
+# chsh -s /bin/zsh
