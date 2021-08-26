a77cacd37b4a27e68b4bced999d4d975773869d4
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2021-08-26 14:05:06 +0200

    Attempt to rely on main branch in repositories

--- setup-git.sh
+++ setup-git.sh
@@ -4,8 +4,8 @@ echo "Set git attributes"
 # Existing diffs https://github.com/git/git/blob/master/userdiff.c
 cp .gitattributes ~/.gitattributes
 git config --global core.attributesfile ~/.gitattributes
-git config --global alias.cleanup "!git branch --merged | grep  -v '\\*\\|master\\|dev' | xargs -n 1 git branch -d"
-git config --global alias.cleanup-remote "!git fetch origin -p && git branch -r --list 'origin/*' --merged origin/master | egrep -v '(^\*|master|dev)' | sed 's/origin\///' | xargs -n 1 git push origin --delete"
+git config --global alias.cleanup "!git branch --merged | grep  -v '\\*\\|master\\|main\\|dev' | xargs -n 1 git branch -d"
+git config --global alias.cleanup-remote "!git fetch origin -p && git branch -r --list 'origin/*' --merged origin/main | egrep -v '(^\*|main|dev)' | sed 's/origin\///' | xargs -n 1 git push origin --delete"
 
 # Configure git for golang to treat repositories as git ones not mercurial:
 git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
